05bd5d9f1f839a48cd5af0238a1e9621b6b405d3
psm651 <42536633+psm651@users.noreply.github.com>
2021-02-23 18:40:43 +0900

    Update user_sync.php

--- user_sync.php
+++ user_sync.php
@@ -13,19 +13,29 @@ chown(_SAVE_PATH_YM, "nobody");
 chgrp(_SAVE_PATH_YM, "nobody");
 
 //필요값
-define("_DOMAIN", ""); //도메인
+#########################################
+
+define("_DOMAIN", "hoseo.edu"); //도메인
+
 define("_ROOT_ORGAN", ""); // 업체명(조직도 루트)
 define("_DELEMETER", ""); // 데이터 구분자
+define("_CHECK_SYNC_COLUMN_LIST", ['name','sosok','gradename','groupname']); // 해당 데이터 변경시 데이터 업데이트(기본 : 이름, 소속, 직급, 그룹)
+define("_EXCEPT_GROUPNAME", []); // groupname 건너뛰기(array) ex)학생, 학부생, etc
 
 define("_GROUP_DATA_JSON", ""); //조직도 데이터 json
 define("_GROUP_DATA_CSV", ""); //조직도 데이터 csv
-define("_IGNORE_EXCEPT_GROUP_DATA", ""); // 조직도 예외데이터 처리방법
+define("_IGNORE_EXCEPT_GROUP_DATA", TRUE); // 조직도 예외데이터 처리방법
 
 define("_USER_DATA_JSON", ""); //사용자 데이터 json
 define("_USER_DATA_CSV", ""); //사용자 데이터 csv
-define("_IGNORE_EXCEPT_USER_DATA", ""); // 사용자 예외데이터 처리방법
+define("_IGNORE_EXCEPT_USER_DATA", TRUE); // 사용자 예외데이터 처리방법
+define("_EXCEPT_USER_COLUMN", ""); // 사용자 예외데이터 처리방법
+define("_ALL_USER_UPDATE", FALSE); // 사용자 예외데이터 처리방법
+
+#########################################
 
 
+define("_DBS_PATH", "/home/mail/domaindir/" . _DOMAIN  . "/_DBS/accounts.dbs");
 
 // 조직도 컬럼 매핑
 $group_mapping_list=[
@@ -34,7 +44,7 @@ $group_mapping_list=[
     'parentcode'  =>  '',
     'depth'  =>  '',
     'sortorder'  =>  '',
-];
+    ];
 
 // 사용자 컬럼 매핑
 $user_mapping_list=[
@@ -42,19 +52,20 @@ $user_mapping_list=[
     'sabeon'  =>  '', //사번
     'sosok'  =>  '', // 부서, 소속 seq
     'sosokname'  =>  '', //부서, 소속 이름
-    'sosok_order'  =>  '', //부서내 사용자 순서
+    'sosok_order'  => '', //부서내 사용자 순서
+    'name'  =>  '', //부서내 사용자 순서
     'hp'  =>  '', //핸드폰
     'tel'  =>  '', //전화
     'email'  =>  '', //email
     'gradename'  =>  '', //직급
     'groupname'  =>  '', //그룹
-];
+    ];
 
 //그룹데이터 동기화
-sync_group_data();
+#sync_group_data($group_mapping_list);
 
 //회원 데이타
-sync_user_data();
+sync_user_data($user_mapping_list);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -63,17 +74,21 @@ function sync_group_data($group_mapping_list)
     //조직도 데이타
     unset($group);
     if (!empty(_GROUP_DATA_JSON)) {
-        $data_group = file_get_contents(_GROUP_DATA_JSON);
-        $list_arr = json_decode($data_group, true);
+        $group_data = file_get_contents(_GROUP_DATA_JSON);
+        $type = 'json';
     } else {
-        $data_group = file_get_contents(_GROUP_DATA_CSV);
+        $group_data = file_get_contents(_GROUP_DATA_CSV);
+        $type = 'csv';
     }
+    $list_arr = get_list_arr($group_data, $type);
 
+    //조직도 root
     $root_code = 0;
-    $sync_start = TRUE;
     $key = array_search(_ROOT_ORGAN, array_column($list_arr, 'name'));
     $root_code = $list_arr[$key]['code'];
-    $cotrol_depth_level = $list_arr[$key]['depth'];
+    $control_depth_level = $list_arr[$key]['depth'];
+    $sync_start = TRUE;
+    $error_list = [];
 
     foreach ($list_arr as $row) {
         $row=(array)$row;
@@ -88,32 +103,43 @@ function sync_group_data($group_mapping_list)
             $row['parentcode'] = '1';
         }
 
+        //매핑값 예외처리 및 예외처리방법
         $column_list=array_keys(array_filter($group_mapping_list));
-        $error_list = [];
         foreach ($column_list as $column) {
-            if(empty($row[$group_mapping_list[$column]])){
-                $error_list[] = $row;
-                if(_IGNORE_EXCEPT_GROUP_DATA == FALSE){
-                    continue;
-                }
-            }
             if(in_array($column,['code','parentcode','sortorder'])){
                 $put_data[$column] = code_conv($row[$group_mapping_list[$column]]);
-            }else if(in_array($column,['code','parentcode','sortorder'])){
+            }else if(in_array($column,['name'])){
                 $put_data[$column] = check_encoding($row[$group_mapping_list[$column]]);
             }else if($column == 'depth'){
-                $put_data[$column] = ($row[$group_mapping_list[$column]] - $cotrol_depth_level);
+                $put_data[$column] = ($row[$group_mapping_list[$column]] - $control_depth_level);
+            }
+
+            if(empty($row[$group_mapping_list[$column]])){
+                $error_list[$column.' is null'] = $row;
+                if(_IGNORE_EXCEPT_GROUP_DATA == FALSE){
+                    break;
+                }
             }
         }
 
+        //연동 될 조직도
         if ($put_data['code']) {
-            $group[] = $put_data;
+            $group[] = array_filter($put_data);
         }
     }
+
+    foreach ((array) $group as $key => $value) {
+        $sort[$key] = $value['depth'];
+    }
+    array_multisort($sort, SORT_ASC, $group);
+
+    //예외상황 처리방법
     if( !empty($error_list) && _IGNORE_EXCEPT_GROUP_DATA == FALSE ){
         $sync_start = FALSE;
     }
+    file_put_contents('./error_list.txt', print_r($error_list, true));
 
+    //실제 api전송
     if ($group && $sync_start) {
         file_put_contents(_SAVE_PATH_YM . "/" . _DOMAIN . "_group_" . _FILE_TIME, json_encode($group), FILE_APPEND);
         @unlink(_SAVE_PATH . "/" . _DOMAIN . "_group");
@@ -127,76 +153,98 @@ function sync_group_data($group_mapping_list)
     }
 }
 
-function sync_user_data()
+function sync_user_data($user_mapping_list)
 {
-    $data_member = file_get_contents(_USER_DATA);
-    file_put_contents('./user_list_file', $data_member);
-    $list_arr = json_decode($data_member, true);
+    unset($group);
+    if (!empty(_USER_DATA_JSON)) {
+        $user_data = file_get_contents(_USER_DATA_JSON);
+        $type = 'json';
+    } else {
+        $user_data = file_get_contents(_USER_DATA_CSV);
+        $type = 'csv';
+    }
+    //sync 될 유저 리스트
+    $list_arr = get_list_arr($user_data, $type);
 
-    $dbs_path = "/home/mail/domaindir/" . _DOMAIN  . "/_DBS/accounts.dbs";
-    $user_list = getUserList($dbs_path);
+    //기존 db유저 리스트
+    $user_list = get_user_list(_DBS_PATH);
+    $sosok_list = get_sosok_list(_DBS_PATH);
 
     unset($member);
+    $sync_start = TRUE;
+    $error_list = [];
+    $column_list=array_flip(array_filter($user_mapping_list, 'myFilter'));
+
     foreach ($list_arr as $key => $user_data) {
+        $user_data =(array)$user_data;
         unset($put_data);
         unset($arr);
-        $sabeon = $user_data['sabeon'];
-        $chk = false;
-        #$chk = true;
+        $sabeon = $user_data[$user_mapping_list['sabeon']];
 
+        // 예외 그룹네임 처리
+        if (in_array($user_data['groupname'], _EXCEPT_GROUPNAME)) {
+            continue;
+        }
+        //컬럼에 대한 예외처리
         foreach ($user_data as $column => $value) {
-            $put_data[$column] = $value;
+            $chk_column = $column_list[$column];
+            if(empty($user_data[$column])){
+                $error_list[$chk_column.'_ERROR'][] = $user_data;
+                if(_IGNORE_EXCEPT_USER_DATA == FALSE){
+                    break;
+                }
+            }
+
+            if(in_array($chk_column,['hp','tel'])){
+                $value = str_replace(" ", "-", $value);
+                if (chk_phone_format($value)) {
+                    $error_list[$chk_column. '_ERROR'][] = $user_data;
+                    $value = "";
+                }
+            }else if(in_array($chk_column,['email'])){
+                $email = $user_list[$sabeon]['id'] . "@" . _DOMAIN;
+                $value = str_replace(" ", "", $value);
+                //자사 이메일인 경우, 이메일 형식 아닐경우 예외 처리
+                if (!chk_email_format($value) || $value == $email ) {
+                    $error_list['same_mailplug_mail'][] = $user_data;
+                    $value = "";
+                }
+            }else if(in_array($chk_column,['sosok', 'sosokname'])){
+                if (!in_array($user_data[$column], array_column($sosok_list, $chk_column))) {
+                    $error_list['move_nososok'][] = $user_data;
+                    unset($user_data[$user_mapping_list['sosokname']]);
+                    unset($user_data[$user_mapping_list['sosok']]);
+                }
+
+                if($chk_column == 'sosok' && !empty($user_data[$column])){
+                    $value = code_conv($value);
+                }
+            }
+            $put_data[$chk_column] = $value;
         }
 
-        $put_data['hp'] = str_replace(" ", "-", $put_data['hp']);
-        $put_data['tel'] = str_replace(" ", "-", $put_data['tel']);
-        $put_data['email'] = str_replace(" ", "", trim($put_data['email']));
-        $put_data['sosok'] = code_conv($put_data['sosok']);
-        $put_data['sosok_order'] = code_conv($put_data['sosok_order']);
-
-
-        if ($put_data['hp'] != "" && chk_phone_format($put_data['hp'])) {
-            $put_data['hp'] = "";
-        }
-        if ($put_data['tel'] != "" && chk_phone_format($put_data['tel'])) {
-            $put_data['tel'] = "";
-        }
-        if ($put_data['email'] != "" && chk_email_format($put_data["email"])) {
-            $put_data['email'] = "";
-        }
-
-        //자사 이메일인 경우 예외 처리
-        $email = $user_list[$sabeon]['id'] . "@" . _DOMAIN;
-        if ($put_data['email'] == $email)
-            $put_data['email'] = "";
-        //전화 번호 변경시
-        // if($user_list[$sabeon]['hp'] != $put_data['hp'])
-        // 	$chk = true;
-        // if($user_list[$sabeon]['email'] != $put_data['email'])
-        // 	$chk = true;
-
-        // 이름이 변경된 경우 업데이트
-        if ($put_data['name'] != "" && $user_list[$sabeon]['name'] != $put_data['name'])
-            $chk = true;
-        // 소속이 변경된 경우 업데이트
-        if ($put_data['sosok'] != "" && $user_list[$sabeon]['sosok'] != $put_data['sosok'])
-            $chk = true;
-        // 직급이 변경된 경우 업데이트
-        if ($put_data['gradename'] != "" && $user_list[$sabeon]['title'] != $put_data['gradename'])
-            $chk = true;
-        // 그룹이 변경된 경우 업데이트
-        if ($put_data['groupname'] != "" && $user_list[$sabeon]['gr_name'] != $put_data['groupname'])
-            $chk = true;
-
-        if ($chk) {
+        //새로 갱신 필요한 인원 추출
+        if (need_to_update($put_data, $user_list, $sabeon)) {
             $put_data['id'] = $user_list[$sabeon]['id'];
             $member[] = $put_data;
         }
     }
 
-    if ($member) {
+    //예외상황 처리방법
+    if( !empty($error_list) && _IGNORE_EXCEPT_USER_DATA == FALSE ){
+        $sync_start = FALSE;
+    }
+
+    file_put_contents('./error_list.txt', print_r($error_list, true));
+    $result_list['user_count'] = count($member);
+    $result_list['sosok_list'] = array_values(array_unique(array_column($member,'sosokname')));
+    $result_list['group_list'] = array_values(array_unique(array_column($member,'groupname')));
+    file_put_contents('./result_list.txt', print_r($result_list, true));
+
+    //실제 api전송
+    if ($member && $sync_start) {
         // 소속 업데이트
-        $dbconn = new SQLite3($dbs_path);
+        $dbconn = new SQLite3(_DBS_PATH);
         $dbconn->busyTimeout(30000);
 
         foreach ($member as $user_info) {
@@ -220,7 +268,7 @@ function sync_user_data()
 }
 
 // 사용자 가져오기
-function getUserList($dbsfile)
+function get_user_list($dbsfile)
 {
     $arr = array();
     if (!file_exists($dbsfile)) {
@@ -229,22 +277,22 @@ function getUserList($dbsfile)
     $dbconn = new SQLite3($dbsfile);
     $dbconn->busyTimeout(30000);
     $sql = "
-		SELECT ac.ac_id AS seq
-		, ac.ac_userid AS id
-		, ac.ac_name AS name
-		, ai.ai_employee_number AS sabeon
-		, ai.ai_gr_name AS gr_name
-		, ac.ac_active AS active
-		, ac.ac_u_priv AS type
-		, ai.ai_email AS email
-		, ac.or_id AS sosok
-		, CASE WHEN ai.ai_or_name='__NOSOSOK__' THEN '소속없음' WHEN ai.ai_or_name is null THEN '소속없음' ELSE ai.ai_or_name END AS or_name
-		, ai.ai_ps_name AS title
-		, ai.ai_join_type AS join_type
-		, ai.ai_hp AS hp
-		FROM accounts AS ac LEFT JOIN accounts_info AS ai ON (ac.ac_id = ai.ac_id)
-		WHERE ac.ac_userid != 'postmaster' and  ac.ac_active IN ('Y','H','G')
-	";
+        SELECT ac.ac_id AS seq
+        , ac.ac_userid AS id
+        , ac.ac_name AS name
+        , ai.ai_employee_number AS sabeon
+        , ai.ai_gr_name AS groupname
+        , ac.ac_active AS active
+        , ac.ac_u_priv AS type
+        , ai.ai_email AS email
+        , ac.or_id AS sosok
+        , CASE WHEN ai.ai_or_name='__NOSOSOK__' THEN '소속없음' WHEN ai.ai_or_name is null THEN '소속없음' ELSE ai.ai_or_name END AS or_name
+        , ai.ai_ps_name AS gradename
+        , ai.ai_join_type AS join_type
+        , ai.ai_hp AS hp
+        FROM accounts AS ac LEFT JOIN accounts_info AS ai ON (ac.ac_id = ai.ac_id)
+        WHERE ac.ac_userid != 'postmaster' and  ac.ac_active IN ('Y','H','G')
+        ";
     $userQuery = $dbconn->query($sql);
     if ($userQuery) {
         while ($data = $userQuery->fetchArray(SQLITE3_ASSOC)) {
@@ -291,33 +339,33 @@ function chk_email_format($user_email)
 function code_conv($code)
 {
     $key_map = array(
-        "A" => "11",
-        "B" => "12",
-        "C" => "13",
-        "D" => "14",
-        "E" => "15",
-        "F" => "16",
-        "G" => "17",
-        "H" => "18",
-        "I" => "19",
-        "J" => "20",
-        "K" => "21",
-        "L" => "22",
-        "M" => "23",
-        "N" => "24",
-        "O" => "25",
-        "P" => "26",
-        "Q" => "27",
-        "R" => "28",
-        "S" => "29",
-        "T" => "30",
-        "U" => "31",
-        "V" => "32",
-        "W" => "33",
-        "X" => "34",
-        "Y" => "35",
-        "Z" => "36",
-    );
+            "A" => "11",
+            "B" => "12",
+            "C" => "13",
+            "D" => "14",
+            "E" => "15",
+            "F" => "16",
+            "G" => "17",
+            "H" => "18",
+            "I" => "19",
+            "J" => "20",
+            "K" => "21",
+            "L" => "22",
+            "M" => "23",
+            "N" => "24",
+            "O" => "25",
+            "P" => "26",
+            "Q" => "27",
+            "R" => "28",
+            "S" => "29",
+            "T" => "30",
+            "U" => "31",
+            "V" => "32",
+            "W" => "33",
+            "X" => "34",
+            "Y" => "35",
+            "Z" => "36",
+            );
 
     $arr = str_split($code);
     foreach ($arr as $key => $val) {
@@ -340,4 +388,68 @@ function check_encoding($str, $type = "UTF-8")
     } else {
         return trim($str);
     }
+}
+
+function get_list_arr($data, $type){
+    $list_arr=[];
+    if ($type == 'json') {
+        $data = file_get_contents($data);
+        $list_arr = json_decode($data, true);
+    } else if($type == 'csv'){
+        $data = explode("\n", $data);
+        foreach($data as $key => $line){
+            unset($tmp);
+            $tmp= explode(_DELEMETER, $line);
+            foreach($tmp as $val){
+                $list_arr[$key][] = trim($val);
+            }
+        }
+    }
+    return $list_arr;
+}
+
+function need_to_update($put_data, $user_list, $sabeon)
+{
+
+    if(_ALL_USER_UPDATE){
+        return true;
+    }
+    $chk = false;
+## 위에 선언됨 define("_CHECK_SYNC_COLUMN_LIST", ['name','sosok','gradename','groupname']); // 해당 데이터 변경시 데이터 업데이트(기본 : 이름, 소속, 직급, 그룹)
+    foreach(_CHECK_SYNC_COLUMN_LIST as $val ){
+        if (!empty($put_data[$val]) && $user_list[$sabeon][$val] != $put_data[$val]) {
+            $chk = true;
+            break;
+        }
+    }
+    return $chk;
+}
+
+// 소속 리스트 가져오기
+function get_sosok_list($dbsfile){
+    $arr = array();
+    if(!file_exists($dbsfile)) {
+        return $arr;
+    }
+    $dbconn = new SQLite3($dbsfile);
+    $dbconn->busyTimeout(30000);
+    $sql = "
+        SELECT seq as sosok, sosokname FROM sosok_new
+        ";
+    $groupQuery = $dbconn->query($sql);
+    if($groupQuery)
+    {
+        while($data = $groupQuery->fetchArray(SQLITE3_ASSOC)){
+            if($data['sosokname'] != "")
+            {
+                $arr[] = $data;
+            }
+        }
+    }
+    $dbconn->close();
+    return $arr;
+}
+
+function myFilter($var){
+  return ($var !== NULL && $var !== FALSE && $var !== '');
 }
